7b979d9f718a0f3d165e391f2517ed2588a49898
Carlos Fonseca M <carlfm01@hotmail.com>
2019-01-23 21:51:22 -0600

    Add speech cutter console client

---
+++ Speech.Cutter.Console/Program.cs
@@ -0,0 +1,67 @@
+﻿using Speech.Cutter.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Speech.Cutter.Console
+{
+    class Program
+    {
+        /// <summary>
+        /// Get the value of an argurment.
+        /// </summary>
+        /// <param name="args">Argument list.</param>
+        /// <param name="option">Key of the argument.</param>
+        /// <returns>Value of the argument.</returns>
+        static string GetArgument(IEnumerable<string> args, string option)
+        => args.SkipWhile(i => i != option).Skip(1).Take(1).FirstOrDefault();
+
+        static void Main(string[] args)
+        {
+            string sentences = GetArgument(args, "--sentences");
+            string inputWav = GetArgument(args, "--input");
+            string outputDir = GetArgument(args, "--output");
+            string confidence = GetArgument(args, "--confidence");
+            string lang = GetArgument(args, "--lang");
+            try
+            {
+                using (var _speechCutter = new SpeechCutterClient(lang))
+                {
+                    //this is an example using manually selected sentences, you should use your own way to clean the text
+                    //to achieve the best sentences try to use sentences splitted by , or . 
+                    if (sentences.Contains(","))
+                    {
+                        _speechCutter.CutSentences(
+                            sentences.Split(','),
+                            inputWav,
+                            outputDir,
+                            Convert.ToDouble(confidence),
+                            Formats.Wav);
+                    }
+                    else
+                    {
+                        _speechCutter.CutSentence(
+                            sentences,
+                            inputWav,
+                            outputDir,
+                            Convert.ToDouble(confidence),
+                            Formats.Wav);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private static string[] DummySentences() => new string[]
+        {
+             "as soon as i reached the meeting place i would find out the wagon to which i was assigned",
+             "and if i sat down and said nothing he would probably soon ask me if i wanted anything to eat",
+             "and perhaps nodding to me",
+             "he would usually grumble savagely and profanely about my having been put with his wagon",
+             "after supper i would roll up in my bedding as soon as possible"
+        };
+    }
+}
---
+++ Speech.Cutter/Models/RecognizedSentence.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Speech.Cutter.Models
+{
+    /// <summary>
+    /// Detected sentence model.
+    /// </summary>
+    public class RecognizedSentence
+    {
+        /// <summary>
+        /// Recognized text.
+        /// </summary>
+        public string Sentence { get; set; }
+        /// <summary>
+        /// Confidence of the recognized sentence.
+        /// </summary>
+        public double Confidence { get; set; }
+        /// <summary>
+        /// Start time of the sentence.
+        /// </summary>
+        public TimeSpan Offset { get; set; }
+        /// <summary>
+        /// Duration of the sentence.
+        /// </summary>
+        public TimeSpan Duration { get; set; }
+    }
+}
